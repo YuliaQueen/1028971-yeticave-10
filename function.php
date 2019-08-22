8d6b41796c03449cb8dfe4dab2eb4b0c2bfca941
Yulia <youlka25@yandex.ru>
2019-08-22 17:36:35 +0300

    Update function.php

--- function.php
+++ function.php
@@ -33,19 +33,7 @@ function esc($str) {
     return $text;
 }
 
-//Функция для подсчета оставшегося времени до закрытия лота
 
-//function time_to_end ($arr) {
-//
-//    $time_end = strtotime($arr);
-//    $time_now = time();
-//    $secs_to_end = $time_end - $time_now;
-//    $h_end = str_pad(floor($secs_to_end/3600), 2, '0', STR_PAD_LEFT);
-//    $m_end = str_pad(floor(($secs_to_end % 3600) / 60), 2, '0', STR_PAD_RIGHT);
-//
-//    return $h_end.':'.$m_end;
-//
-//}
 
 function time_to_end($ends_str)
 {
@@ -53,9 +41,9 @@ function time_to_end($ends_str)
     $date_now   = new DateTime();
     $difference = $date_then->diff($date_now, false);
 
-    if ($date_then < $date_now) {
-        return print('Лот завершен!');
-    }
+
+
+
 
     if ($difference->y > 0) { // Если годов минимум 1
         return  $difference->format('%y') . ' г';
@@ -63,21 +51,22 @@ function time_to_end($ends_str)
         return  $difference->format('%m') . ' м';
     } elseif ($difference->d > 0) { // Если дней минимум 1
         return  $difference->format('%d') . ' д';
-    }  else { // Если меньше
+    } else { // Если меньше
         return  $difference->format('%H час. %i мин.');
     }
-
-
 }
 
-//Должна добавлять css-класс timer——finishing в разметку
+//добавляет класс в разметку
 function time_class($ends_str) {
     $date_then  = DateTime::createFromFormat('Y-m-d H:i:s', "$ends_str 23:59:59");
     $date_now   = new DateTime();
+    if($date_then < $date_now) {
+        return 0;
+    }
     $difference = $date_then->diff($date_now, false);
-    $flag = 0;
-    if ($difference <= '1 hour') {
-        $flag = 1;
-        return $flag;
-    } else return '';
+
+    if ($difference -> h <= 1 ) {
+        return 1;
+    } else return 2;
+
 };
