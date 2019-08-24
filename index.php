e82ddef0b308138b230c2aa95cdc29fb36f9636d
Yulia <youlka25@yandex.ru>
2019-08-24 14:26:02 +0300

    Подключение лотов через БД

--- index.php
+++ index.php
@@ -2,26 +2,47 @@
 require('function.php');
 require('data.php');
 require ('init.php');
-
+//подключение категорий
+//если нет подключения к базе, выводим страницу с ошибкой
 if (!$link) {
     $error = mysqli_connect_error();
     $content = include_template('error.php', ['error' => $error]);
 }
 else {
-    $sql = 'SELECT * FROM categories';
+    $sql = 'SELECT * FROM categories';//запрос
 
-    $result = mysqli_query($link, $sql);
+    $result = mysqli_query($link, $sql);//результат запроса
 
     if ($result) {
-        $category = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $category = mysqli_fetch_all($result, MYSQLI_ASSOC);//результат запроса выводим в массив
     }
     else {
-        $error = mysqli_error($link);
+        $error = mysqli_error($link);//если ошибка
         $content = include_template('error.php', ['error' => $error]);
     }
 }
 
+//подключение лотов
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+}
+else {
+    $sql =   'SELECT *, cat.category_name FROM lots JOIN categories AS cat ON lot_category = cat.category_id';
 
+
+    $result = mysqli_query($link, $sql);//результат запроса
+
+    if ($result) {
+        $items_structure = mysqli_fetch_all($result, MYSQLI_ASSOC);//результат запроса выводим в массив
+    }
+    else {
+        $error = mysqli_error($link);//если ошибка
+        $content = include_template('error.php', ['error' => $error]);
+    }
+}
+
+//шаблонизация
 $main_content = include_template('main.php', [
     'items_structure' => $items_structure,
     'category' => $category
--- init.php
+++ init.php
@@ -1,6 +1,10 @@
 <?php
-
+//подключаем базу данных
 $link = mysqli_connect('localhost', 'root', '', 'yeti_cave');
+
+//устанавливаем кодировку
 mysqli_set_charset($link, 'utf8');
+
 $category = [];
 $content = '';
+$items_structure = [];
