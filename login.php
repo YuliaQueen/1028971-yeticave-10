617b9d525dc9746ad0ee05f349fd0edc024670e4
Yulia <youlka25@yandex.ru>
2019-09-07 12:33:06 +0300

    Update login.php

--- login.php
+++ login.php
@@ -7,13 +7,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $form = $_POST;
     $required = ['user_email', 'user_password'];
 
+//проверка на заполненность полей
     foreach ($required as $field) {
         if (empty($form[$field])) {
             $errors[$field] = 'Это поле надо заполнить';
         }
     };
 
+//если массив с ошибками пуст...
     if (empty($errors)) {
+//        $email_in_form = $form['user_email'];
+//        $email = query_scalar("SELECT `user_email` FROM `users` WHERE `user_email` = '$email_in_form'");
+//        var_dump($email);
+//        die();
         header('Location: index.php');
     };
 
